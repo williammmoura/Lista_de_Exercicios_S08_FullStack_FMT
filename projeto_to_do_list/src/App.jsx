7a73dc5a259e4ea99d8ec7af5a5f9b0a35834daa
William Monte de Moura <williamonte@gmail.com>
2023-04-09 21:16:35 -0300

    Exercício 07

--- projeto_to_do_list/src/App.jsx
+++ projeto_to_do_list/src/App.jsx
@@ -3,17 +3,20 @@ import './App.css'
 import ListItem from './components/ListItem'
 
 function App() {
-  //Estado base
+  
+  // Estado inicial
   const [listaDeTarefas, setListaDeTarefas] = useState([
     {id:1, texto:"Tarefa 1", finalizado:false}
   ])
 
+  // Função que adiciona uma nova tarefa à lista de tarefas.
   function adicionarTarefa(tarefa){
     const novaTarefa = {id:listaDeTarefas.length + 1, texto: tarefa, finalizado: false}
 
     setListaDeTarefas([...listaDeTarefas, novaTarefa])
   }
 
+  // Função que remove uma tarefa da lista de tarefas.
   function removerTarefa(tarefa){
     //Filtrar (filter) a tarefa selecionada e assim remover a tarefa
     const novaLista = listaDeTarefas.filter(item => item.id != tarefa.id)
--- projeto_to_do_list/src/components/ListItem.jsx
+++ projeto_to_do_list/src/components/ListItem.jsx
@@ -1,27 +1,45 @@
 import React, { useState } from "react";
 
-function ListItem(props){
+function ListItem({tarefa, removerTarefa}){
     
     //Estado inicial do componente, que indicará se a tarefa foi finalizado ou não.
-    const [finalizado, setFinalizado] = useState(props.finalizado);
+    const [finalizado, setFinalizado] = useState(tarefa.finalizado);
 
-    //Atualizando o estado para "true" quando a tarefa for finalizada
-    const handleFinalizado = () => {
-        setFinalizado(true);
-    };
+    // Finalizando a tarefa
+    function finalizarTarefa(evento){
+        evento.preventDefault()
+        setFinalizado(!finalizado)
+    }
+
+    // Chamando a função para remover a tarefa
+    function handleRemoverTarefa(evento){
+        evento.preventDefault()
+        removerTarefa(tarefa)
+    }
 
     //Conteúdo que será renderizado na tela
     return(
         <div>
             
-            {/* Nome da tarefa */}
-            <h3>{props.tarefa}</h3>
+            {
+                // renderização condicional
+                finalizado ? (
+                    // <li> com marcação de finalizado
+                    <li key={tarefa.id} className={'list-item finalizado'}>
+                        {tarefa.texto}
+                        <button className="list-item-button" onClick={finalizarTarefa}>finalizar</button>
+                        <button className="list-item-button" onClick={handleRemoverTarefa}>remover</button>
+                    </li>
+                ) : (
+                    // <li> sem marcação de finalizado
+                    <li key={tarefa.id} className={'list-item'}>
+                        {tarefa.texto}
+                        <button className="list-item-button" onClick={finalizarTarefa}>finalizar</button>
+                        <button className="list-item-button" onClick={handleRemoverTarefa}>remover</button>
+                    </li>
+                )
+            }
 
-            {/* Mensagem para indicar se a tarefa foi finalizada ou não. */}
-            <p>{finalizado ? "Finalizado" : "Não finalizado"}</p>
-
-            {/* Botão para finalizar a tarefa */}
-            <button onClick={handleFinalizado}>Finalizar Tarefa</button>
         </div>
     )
 }
